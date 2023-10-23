b2bec4d10a82bb937c170eb77708e6b33d7c55e2
Oleh Bohatov <obohatov@gmail.com>
2023-10-23 12:23:39 +0200

    Add models, serializers and views in user app

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -21,12 +21,12 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
